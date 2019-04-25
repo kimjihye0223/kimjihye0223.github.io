690109e3531cb8f7ee7856ba53796b440567b36c
kimjihye0223233 <45805720+kimjihye0223233@users.noreply.github.com>
2019-04-25 17:35:00 +0900

    Add files via upload

--- portfolio/script/imgload.js
+++ portfolio/script/imgload.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         var intv = setInterval(function(){ nextAni(); }, 10000);
         
         function nextAni(){
-               $(".collection").not(":animated").animate({"margin-top":-hei+"px"}, 3000, function(){
+               $(".collection").not(":animated").animate({"margin-top":-hei+"px"}, 800, function(){
                    $(".generbox").eq(0).appendTo($(".collection"));
                    $(".collection").css("margin-top", "0px");
                    $(".numBtnFrame li").eq(0).appendTo($(".numBtnFrame"));
@@ -15,7 +15,7 @@ $(document).ready(function(){
         function prevAni(){
                $(".generbox").eq(cnt).prependTo($(".collection"));
                $(".collection").css("margin-top", -hei+"px");
-               $(".collection").not(":animated").animate({"margin-top":"0px"}, 1000, function(){
+               $(".collection").not(":animated").animate({"margin-top":"0px"}, 800, function(){
                    $(".numBtnFrame li").eq(cnt).prependTo($(".numBtnFrame"));
                    $(".numBtnFrame li button").removeClass("on");
                    $(".numBtnFrame li").eq(0).find("button").addClass("on");
